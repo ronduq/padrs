bb1dac90b25b67da4f2fb78d277096e9cb1275b7
raduFriday <radu.silaghi@wearefriday.com>
2020-09-17 14:45:02 +0100

    update mobile menu

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,52 +1,7 @@
 import Search from './components/search';
+import MobileMenu from './components/mobileMenu';
 
 const form = document.querySelector('form[action="/jobs"][data-enabled]');
 if (form) new Search(form);
 
-
-const firstFocusableEl = document.querySelector('.mobile-menu__first-link'),
-      lastFocusableEl = document.querySelector('.mobile-menu__last-link'),
-      mobileButton = document.querySelector('.mobile-menu__button'),
-      mobileLinks = document.querySelectorAll('.mobile-menu__link'),
-      KEYCODE_TAB = 9,
-      menuButton = document.querySelector('.mobile-menu__button')
-let ariaExpanded = menuButton.getAttribute('aria-expanded');
-
-document.addEventListener('keydown', function(e) {
-    let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-
-    if (!isTabPressed || !ariaExpanded) { 
-        return; 
-    }
-    console.log(ariaExpanded)
-    if (isTabPressed && !ariaExpanded) { 
-      for (let i=0; i < mobileLinks.length; i++) {
-        mobileLinks[i].tabIndex = -1;
-      }
-      return; 
-    }
-
-    if (ariaExpanded) {
-      for (let i=0; i < mobileLinks.length; i++) {
-        mobileLinks[i].tabIndex = 0;
-      }
-    }
-
-    if ( e.shiftKey ) /* shift + tab */ {
-        if (document.activeElement === firstFocusableEl) {
-            lastFocusableEl.focus();
-            e.preventDefault();
-        }
-    } else /* tab */ {
-        if (document.activeElement === lastFocusableEl) {
-            firstFocusableEl.focus();
-            e.preventDefault();
-        }
-    }
-
-});
-
-menuButton.addEventListener('click',() => {
-  ariaExpanded = !ariaExpanded;
-  menuButton.setAttribute("aria-expanded", ariaExpanded);
-})
+new MobileMenu();
--- src/site/assets/javascripts/application.js
+++ src/site/assets/javascripts/application.js
@@ -3984,62 +3984,81 @@ module.exports = str => encodeURIComponent(str).replace(/[!'()*]/g, x => `%${x.c
 
 var _search = _interopRequireDefault(require("./components/search"));
 
+var _mobileMenu = _interopRequireDefault(require("./components/mobileMenu"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var form = document.querySelector('form[action="/jobs"][data-enabled]');
 if (form) new _search["default"](form);
-var firstFocusableEl = document.querySelector('.mobile-menu__first-link'),
-    lastFocusableEl = document.querySelector('.mobile-menu__last-link'),
-    mobileButton = document.querySelector('.mobile-menu__button'),
-    mobileLinks = document.querySelectorAll('.mobile-menu__link'),
-    KEYCODE_TAB = 9,
-    menuButton = document.querySelector('.mobile-menu__button');
-var ariaExpanded = menuButton.getAttribute('aria-expanded');
-document.addEventListener('keydown', function (e) {
-  var isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
+new _mobileMenu["default"]();
 
-  if (!isTabPressed || !ariaExpanded) {
-    return;
-  }
+},{"./components/mobileMenu":7,"./components/search":8}],7:[function(require,module,exports){
+"use strict";
 
-  console.log(ariaExpanded);
-
-  if (isTabPressed && !ariaExpanded) {
-    for (var i = 0; i < mobileLinks.length; i++) {
-      mobileLinks[i].tabIndex = -1;
-    }
-
-    return;
-  }
-
-  if (ariaExpanded) {
-    for (var _i = 0; _i < mobileLinks.length; _i++) {
-      mobileLinks[_i].tabIndex = 0;
-    }
-  }
-
-  if (e.shiftKey)
-    /* shift + tab */
-    {
-      if (document.activeElement === firstFocusableEl) {
-        lastFocusableEl.focus();
-        e.preventDefault();
-      }
-    } else
-    /* tab */
-    {
-      if (document.activeElement === lastFocusableEl) {
-        firstFocusableEl.focus();
-        e.preventDefault();
-      }
-    }
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-menuButton.addEventListener('click', function () {
-  ariaExpanded = !ariaExpanded;
-  menuButton.setAttribute("aria-expanded", ariaExpanded);
-});
+exports["default"] = void 0;
 
-},{"./components/search":7}],7:[function(require,module,exports){
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var MobileMenu = function MobileMenu() {
+  _classCallCheck(this, MobileMenu);
+
+  var firstFocusableEl = document.querySelector('.mobile-menu__first-link'),
+      lastFocusableEl = document.querySelector('.mobile-menu__last-link'),
+      mobileButton = document.querySelector('.mobile-menu__button'),
+      mobileLinks = document.querySelectorAll('.mobile-menu__link'),
+      KEYCODE_TAB = 9,
+      menuButton = document.querySelector('.mobile-menu__button');
+  var ariaExpanded = menuButton.getAttribute('aria-expanded');
+  document.addEventListener('keydown', function (e) {
+    var isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
+
+    if (!isTabPressed || !ariaExpanded) {
+      return;
+    }
+
+    if (isTabPressed && ariaExpanded === false) {
+      return;
+    }
+
+    if (e.shiftKey)
+      /* shift + tab */
+      {
+        if (document.activeElement === firstFocusableEl) {
+          lastFocusableEl.focus();
+          e.preventDefault();
+        }
+      } else
+      /* tab */
+      {
+        if (document.activeElement === lastFocusableEl) {
+          firstFocusableEl.focus();
+          e.preventDefault();
+        }
+      }
+  });
+  menuButton.addEventListener('click', function () {
+    ariaExpanded = !ariaExpanded;
+    menuButton.setAttribute("aria-expanded", ariaExpanded);
+
+    if (ariaExpanded) {
+      for (var i = 0; i < mobileLinks.length; i++) {
+        mobileLinks[i].tabIndex = 0;
+      }
+    } else {
+      for (var _i = 0; _i < mobileLinks.length; _i++) {
+        mobileLinks[_i].tabIndex = -1;
+      }
+    }
+  });
+};
+
+var _default = MobileMenu;
+exports["default"] = _default;
+
+},{}],8:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
