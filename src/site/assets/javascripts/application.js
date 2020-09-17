a0e4cefb6c1d9d2f5c53b1018e82bf404b639388
raduFriday <radu.silaghi@wearefriday.com>
2020-09-17 09:53:18 +0100

    update design and a11y

--- src/site/assets/javascripts/application.js
+++ src/site/assets/javascripts/application.js
@@ -3988,6 +3988,52 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 var form = document.querySelector('form[action="/jobs"][data-enabled]');
 if (form) new _search["default"](form);
+var firstFocusableEl = document.querySelector('.mobile-menu__first-link'),
+    lastFocusableEl = document.querySelector('.mobile-menu__last-link'),
+    mobileButton = document.querySelector('.mobile-menu__button'),
+    mobileLinks = document.querySelectorAll('.mobile-menu__link'),
+    KEYCODE_TAB = 9;
+document.addEventListener('keydown', function (e) {
+  var isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
+  console.log('gen key', mobileButton.checked);
+
+  if (!isTabPressed || !mobileButton.checked) {
+    return;
+  }
+
+  console.log(mobileButton.checked);
+
+  if (isTabPressed && !mobileButton.checked) {
+    for (var i = 0; i < mobileLinks.length; i++) {
+      mobileLinks[i].tabIndex = -1;
+    }
+
+    console.log('mobile butotn not checked');
+    return;
+  }
+
+  if (mobileButton.checked) {
+    for (var i = 0; i < mobileLinks.length; i++) {
+      mobileLinks[i].tabIndex = 0;
+    }
+  }
+
+  if (e.shiftKey)
+    /* shift + tab */
+    {
+      if (document.activeElement === firstFocusableEl) {
+        lastFocusableEl.focus();
+        e.preventDefault();
+      }
+    } else
+    /* tab */
+    {
+      if (document.activeElement === lastFocusableEl) {
+        firstFocusableEl.focus();
+        e.preventDefault();
+      }
+    }
+});
 
 },{"./components/search":7}],7:[function(require,module,exports){
 "use strict";
